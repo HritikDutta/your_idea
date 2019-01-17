c6f1d7144c8fd60759633a1c03a5fcb8c14f93be
ThreewayDude <32474626+ThreewayDude@users.noreply.github.com>
2019-01-17 16:18:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const postController = require('./controllers/post')
-const createProfileController = require('./controllers/profile')
 
 const app = express()
 
@@ -9,6 +8,5 @@ app.set('view engine', 'ejs')
 app.use(express.static('./views/'))
 
 postController(app)
-createProfileController(app)
 
 app.listen(3000)
